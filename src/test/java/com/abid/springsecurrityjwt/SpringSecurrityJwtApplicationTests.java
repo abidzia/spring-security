1ca67675971489659494e43742d93bc204d77759
abidzia <abidzia.qau@gmail.com>
2024-05-02 16:54:17 +0500

    Security Practice # 1
    Basic UserName and Password implementation with Bycrypt encoding

---
+++ src/test/java/com/abid/springsecurrityjwt/SpringSecurrityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abid.springsecurrityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurrityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
